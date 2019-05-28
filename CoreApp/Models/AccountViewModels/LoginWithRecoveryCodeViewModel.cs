f84908b1f666708317992c1523ea908f2b115e58
Tống Gia Vĩnh <tonggiavinh@outlook.com>
2019-05-28 15:11:45 +0700

    Inital Project

---
+++ CoreApp/Models/AccountViewModels/LoginWithRecoveryCodeViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CoreApp.Models.AccountViewModels
+{
+    public class LoginWithRecoveryCodeViewModel
+    {
+        [Required]
+        [DataType(DataType.Text)]
+        [Display(Name = "Recovery Code")]
+        public string RecoveryCode { get; set; }
+    }
+}
